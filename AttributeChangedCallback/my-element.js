9979af25ff204397d030b9a0b5eed24dea6846f7
Santiago G <sgonzalez@modyo.com>
2022-12-06 20:10:02 -0500

    feat: added attributeChangedCallback

---
+++ AttributeChangedCallback/my-element.js
@@ -0,0 +1,46 @@
+class myElement extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+
+	}
+	static get observedAttributes() {
+		return ['title', 'parrafo', 'img'];
+	}
+	attributeChangedCallback(attr, oldVal, newVal) {
+		if (oldVal !== newVal) {
+			this[attr] = newVal
+		}
+	}
+	getTemplate() {
+		const template = document.createElement("template");
+		template.innerHTML = `
+			<section>
+				<h2>${this.title}</h2>
+				<div>
+					<p>${this.parrafo}</p>
+					<img src="${this.img}" alt="" />
+				</div>
+			</section>
+			${this.getStyles()}
+		`;
+		return template;
+	}
+	getStyles() {
+		return `
+			<style>
+				h2 {
+					color: red;
+				}
+			</style>
+		`;
+	}
+	render() {
+		this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+	}
+	connectedCallback() {
+		this.render();
+	}
+}
+
+customElements.define('my-element', myElement);
